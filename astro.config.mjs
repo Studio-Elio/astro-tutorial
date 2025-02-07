5e8447d36ffedb7b338ab7b650be4badf887e8f3
jyhl <circularboxes@gmail.com>
2025-02-07 13:38:23 -0800

    Implement storyblok for team page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aquamarine-tapioca-0d0396.netlify.app",
-  integrations: [preact()]
+    site: "https://aquamarine-tapioca-0d0396.netlify.app",
+    integrations: [
+        preact(),
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                teamMember: "storyblok/TeamMember",
+            },
+            apiOptions: {
+                region: "us", // optional,  or 'eu' (default)
+            },
+        }),
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        },
+    },
 });
